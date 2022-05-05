7f23b9cd4ca774aa23bf63e957299ff9c7bfd7cf
jxlefort <jxlefort@gmail.com>
2022-05-05 13:32:42 -0400

    initial commit

---
+++ React-Portfolio-main/src/components/Header/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Header() {
+    return (
+        <section>
+            <a href='/'>
+                <h1 id="header">Zack Greenfield's Portfolio</h1>
+            </a>
+        </section>
+    );
+}
+
+export default Header;
